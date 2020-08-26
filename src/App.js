d7ee2513a40dfd2843d2c7e66db09bd1c786822b
Kaustuv DeBiswas <kaustuv@users.noreply.github.com>
2020-08-26 14:40:57 +0530

    refac: remove unused file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef } from "react";
 import { elemsData } from "./data";
 import * as _ from "lodash";
 import "./styles.css";
